a0d6db3003da3c073b8c117eb2779eb94370437a
herytrie <hery.trie@gmail.com>
2014-04-14 22:55:01 +0700

    Update db.php

--- includes/db.php
+++ includes/db.php
@@ -2,7 +2,7 @@
 define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_DATABASE', 'testians');
+define('DB_DATABASE', 'isuwer');
 $connection = mysql_connect(DB_SERVER, DB_USERNAME, DB_PASSWORD) or die(mysql_error());
 $database = mysql_select_db(DB_DATABASE) or die(mysql_error());
 mysql_query ("set character_set_results='utf8'");   
